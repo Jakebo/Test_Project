2bb1bf837596330598dbddf883ef822ccb72ecce
jakebo <jakebo.huang@gmail.com>
2016-02-29 23:16:52 +0800

    Perfect CanDevice Class

--- can_device.cpp
+++ can_device.cpp
@@ -3,7 +3,14 @@
 CanDevice::CanDevice(const Json::Value &can)
     : BaseDevice(can)
 {
-    
+    baudrate = can[DEVICE_BAUDRATE].asUInt();
+    if (can[DEVICE_LOOP] != Json::nullValue)
+        loop     = can[DEVICE_LOOP].asInt();
+}
+
+int CanDevice::CanInit(void)
+{
+    return 0;
 }
 
 bool CanDevice::TestDevice(void)
--- can_device.hpp
+++ can_device.hpp
@@ -11,6 +11,7 @@ typedef uint32_t can_baudrate_t;
 class CanDevice : public BaseDevice {
 public:
     CanDevice(const Json::Value &can);
+    int CanInit(void);
     bool TestDevice(void);
 
 private:
